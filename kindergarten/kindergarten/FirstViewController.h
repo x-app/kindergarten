5b0ad30f3fd3863c0f4a4da4d241ddcf98a0525d
slice <slice@slices-Mac.local>
2015-07-01 21:32:00 +0800

    new project

---
+++ kindergarten/kindergarten/FirstViewController.h
@@ -0,0 +1,15 @@
+//
+//  FirstViewController.h
+//  kindergarten
+//
+//  Created by slice on 15-7-1.
+//  Copyright (c) 2015年 xapp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+
+@end
+
---
+++ kindergarten/kindergarten/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  kindergarten
+//
+//  Created by slice on 15-7-1.
+//  Copyright (c) 2015年 xapp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
