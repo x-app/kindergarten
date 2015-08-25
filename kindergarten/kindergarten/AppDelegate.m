0ac6504ad43303a1b30dc659a91245dba00269f5
Bin WANG <netmad@163.com>
2015-08-25 18:13:54 +0800

    change pushservice url

--- kindergarten/kindergarten/AppDelegate.m
+++ kindergarten/kindergarten/AppDelegate.m
@@ -92,8 +92,10 @@
     self.varible = [[KGVarible alloc] init];
     self.varible.server_index_url = @"http://app.nugget-nj.com/kindergarten_index";
     //self.varible.server_app_url = @"http://app.nugget-nj.com/nugget_app";
-    self.varible.server_push_url = @"http://slice.eu.org:8080/pushservice/api";
-        
+    //self.varible.server_push_url = @"http://slice.eu.org:8080/pushservice/api";
+    self.varible.server_push_url = @"http://app.nugget-nj.com/pushservice/testapi";
+
+    
     NSDictionary *variDict = [userDefaults objectForKey:@"varible"];
     if (variDict != nil) {
         [self.varible fromDictionary:variDict];
